0b3d42752e936db82c6332e5f6b51268fd2c546c
SEMOSS <semoss@semoss.org>
2019-09-17 17:43:33 -0400

    Commited on..  : 2019/09/17 17:43:33

---
+++ assets/sentiment.r
@@ -0,0 +1,18 @@
+score_sentiment<-function(df){
+# Arguments: dataframe with one column: review/optinion        
+# output: a dataframe with the sentiment scores for the respected rows of the dataframe
+
+  library(sentimentr)
+  out<-vector()
+  n<-nrow(df)
+  names(df)<-"review"
+  if(n>0){
+                 for(i in 1:n){
+                               review<-gsub("_"," ",as.character(df$review[i]))
+                               score<-mean(sentiment(review)$sentiment)
+                               out<-append(out,score)
+                 }
+                 df$sentiment<-out
+  }
+  return(df)
+}
